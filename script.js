9fb58a9c7759676f4b43d7d473597698b7a6620b
sriniv0109 <sriniv0109@gmail.com>
2025-03-05 18:10:05 +0530

    capchta updates

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function handleSubmit(){
-let outputDate=document.querySelector(".outputDate");
+let capchaChecked=false;
+if(capchaChecked){
+
+    let outputDate=document.querySelector(".outputDate");
 let inputDate=document.querySelector(".inputDate");
 console.log('inputDate',inputDate.value);//type of this date is String
 
@@ -8,12 +11,22 @@ console.log('inputDate',inputDate.value);//type of this date is String
 let formattedDate=new Date(inputDate.value).toLocaleDateString("en-IN");
 outputDate.value=formattedDate;
 console.log("outputDate.value",outputDate.value);
+
+}
+else{
+    alert("Please check the capcha check box");
+}
+
 }
 function timestamp() { 
 var response = document.getElementById("g-recaptcha-response");
- if (response == null || response.value.trim() == "") {
-             var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
-    elems["ts"] = JSON.stringify(new Date().getTime());
-    document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
+if (response == null || response.value.trim() == "") {
+            var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
+elems["ts"] = JSON.stringify(new Date().getTime());
+document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
 }
- } setInterval(timestamp, 500);
+} setInterval(timestamp, 500);
+
+function capchtacallback(){
+capchaChecked=true;
+}

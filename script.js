141e71e45686e943ed7387ec1004b45933db8813
sriniv0109 <sriniv0109@gmail.com>
2025-03-05 17:51:25 +0530

    capcha added

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 function handleSubmit(){
-    let outputDate=document.querySelector(".outputDate");
-    let inputDate=document.querySelector(".inputDate");
-    console.log('inputDate',inputDate.value);//type of this date is String
-    
-    //we have to convert this date as per user locale
+let outputDate=document.querySelector(".outputDate");
+let inputDate=document.querySelector(".inputDate");
+console.log('inputDate',inputDate.value);//type of this date is String
 
-    let formattedDate=new Date(inputDate.value).toLocaleDateString("en-IN");
-    outputDate.value=formattedDate;
-    console.log("outputDate.value",outputDate.value);
+//we have to convert this date as per user locale
+
+let formattedDate=new Date(inputDate.value).toLocaleDateString("en-IN");
+outputDate.value=formattedDate;
+console.log("outputDate.value",outputDate.value);
 }
+function timestamp() { 
+var response = document.getElementById("g-recaptcha-response");
+ if (response == null || response.value.trim() == "") {
+             var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
+}
+ } setInterval(timestamp, 500);
